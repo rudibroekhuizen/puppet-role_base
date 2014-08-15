256bbd1999d646edc44083fed790e912301fc483
rudibroekhuizen <rudi.broekhuizen@naturalis.nl>
2014-08-15 14:10:39 +0200

    Update packages.pp

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -3,11 +3,11 @@
 class role_base::packages (
   $package_hash_debian,
   $package_hash_redhat,
-  $attribute_defaults,
+  $package_attribute_defaults,
   ){
 
 case $::osfamily {
-  debian: { create_resources('base::packages', $package_hash_debian, $attribute_defaults) }
-  redhat: { create_resources('base::packages', $package_hash_redhat, $attribute_defaults) }
+  debian: { create_resources('base::packages', $package_hash_debian, $package_attribute_defaults) }
+  redhat: { create_resources('base::packages', $package_hash_redhat, $package_attribute_defaults) }
   }
 }
