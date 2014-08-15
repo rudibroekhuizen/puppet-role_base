7e1d2b68bba2e2f85f03e80f05fbf44b56dcb3b9
rudibroekhuizen <rudi.broekhuizen@naturalis.nl>
2014-08-15 15:15:22 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -57,7 +57,7 @@ class role_base (
                              },
                              
   $user_hash = { 'rudi.broekhuizen' => { password => Passw0rd,
-                                         groups   => wheel,
+                                         groups   => admin,
                                          ensure   => present,
                                        },
                },
