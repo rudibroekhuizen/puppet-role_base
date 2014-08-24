89522e6b90aa38745d68786ac1ba60be305db234
rudibroekhuizen <rudi.broekhuizen@naturalis.nl>
2014-08-24 16:48:09 +0200

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Install Puppet and Git on osfamily debian
-if [ -f /usr/bin/dpkg ]
-then
+if [ -f /usr/bin/dpkg ];then
   wget http://apt.puppetlabs.com/puppetlabs-release-stable.deb
   dpkg -i puppetlabs-release-stable.deb
   apt-get --yes --quiet update
@@ -10,8 +9,7 @@ then
 fi
 
 # Install Puppet and Git on osfamily redhat
-if [ -f /bin/rpm ]
-then
+if [ -f /bin/rpm ];then
   rpm -Uhv --force http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
   curl -O http://apt.sw.be/redhat/el6/en/x86_64/extras/RPMS/git-1.7.10-1.el6.rfx.x86_64.rpm
   curl -O http://apt.sw.be/redhat/el6/en/x86_64/extras/RPMS/perl-Git-1.7.10-1.el6.rfx.x86_64.rpm
@@ -21,17 +19,12 @@ then
 fi
 
 # Install r10k and download modules
-if [[ ! `gem list r10k` ]];then
+if [ ! `gem list r10k` ];then
   gem install r10k
 fi
 curl https://raw.githubusercontent.com/rudibroekhuizen/puppet-role_base/master/files/Puppetfile > /etc/puppet/Puppetfile
-
 PUPPETFILE=/etc/puppet/Puppetfile PUPPETFILE_DIR=/etc/puppet/modules r10k --verbose 3 puppetfile install
 #r10k puppetfile install
-
-# Fetch base repository from Github
-git clone https://github.com/rudibroekhuizen/puppet-base /etc/puppet/modules/base
-git clone https://github.com/rudibroekhuizen/puppet-role_base /etc/puppet/modules/role_base
 
 # Apply base module
 puppet apply /etc/puppet/modules/role_base/tests/init.pp
