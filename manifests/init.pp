d89253e115d98c0b41a6cfc88e552ae0cc4cea2e
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-10-17 13:00:19 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -28,7 +28,7 @@ class role_base (
                              
   $users_hash = { 'rudi.broekhuizen' => { comment => 'Rudi Broekhuizen',
                                           email   => 'rudi.broekhuizen@naturalis.nl',
-                                          groups  => ['wheel','adm','dialout','cdrom','plugdev'],
+                                          groups  => ['wheel','sudo','adm','plugdev'],
                                           ensure  => present,
                                           shell   => '/bin/zsh',
                                           ssh_key => { type => "ssh-rsa",
