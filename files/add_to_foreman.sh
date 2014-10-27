a571acb957d33e0a3b69128bb6db15c1488ea500
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-10-27 16:59:04 +0100

    Update add_to_foreman.sh

--- files/add_to_foreman.sh
+++ files/add_to_foreman.sh
@@ -2,13 +2,12 @@
 #kind: finish
 #name: Ubuntu ec2 Finish
 
-#OS:
+#oses:
 #- Debian 6.0
 #- Debian 7.0
 #- Ubuntu 10.04
 #- Ubuntu 12.04
 #- Ubuntu 13.04
-#- Ubuntu 14.04
 #- CentOS 6
 
 environment=1402production
@@ -20,6 +19,28 @@ then
   echo "Can not resolve fqdn, please enter fqdn of host: (example: $hostname.internal.domain)"
   read hostname
 fi
+
+if [ -f /usr/bin/dpkg ]
+then
+  wget http://apt.puppetlabs.com/puppetlabs-release-stable.deb
+  dpkg -i puppetlabs-release-stable.deb
+  apt-get --yes --quiet update
+  apt-get --yes -o Dpkg::Options::="--force-confold" --quiet install git puppet-common puppet libaugeas-ruby
+fi
+
+# centos
+if [ -f /bin/rpm ]
+then
+  rpm -Uhv --force http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
+  curl -O http://apt.sw.be/redhat/el6/en/x86_64/extras/RPMS/git-1.7.10-1.el6.rfx.x86_64.rpm
+  curl -O http://apt.sw.be/redhat/el6/en/x86_64/extras/RPMS/perl-Git-1.7.10-1.el6.rfx.x86_64.rpm
+  curl -O http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+  rpm -i --force epel-release-6*.rpm
+  yum -y install perl-DBI rsync openssh-clients wget
+  rpm -i --force git-1.7.10-1.el6.rfx.x86_64.rpm perl-Git-1.7.10-1.el6.rfx.x86_64.rpm
+  yum -y install puppet
+fi
+
 
 cat << EOF > /etc/puppet/puppet.conf
 #kind: snippet
