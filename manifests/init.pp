e200d380bb46864458c1c76789e673389490a73f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-03-03 09:46:01 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,23 @@
 #
 #
 class role_base (
+  $repos_array_debian,
   $owner_email,
+  $packages_attribute_defaults,
+  $packages_hash_debian,
+  $packages_hash_redhat,
+  $users_attribute_defaults,
+  $users_hash,
   ) {
   
   class { 'base':
-    owner_email => $owner_email  
+    repos_array_debian          => $repos_array_debian,
+    owner_email                 => $owner_email,
+    packages_attribute_defaults => $packages_attribute_defaults,
+    packages_hash_debian        => $packages_hash_debian,
+    packages_hash_redhat        => $packages_hash_redhat,
+    users_attribute_defaults    => $users_attribute_defaults,
+    users_hash                  => $users_hash,
   }
 
 }
