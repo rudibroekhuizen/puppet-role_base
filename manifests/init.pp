0640b49a96ff1f33047eecb227e318fa111a2fc9
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-02-19 12:30:22 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,24 +35,8 @@
 #
 # Copyright 2014 Your name here, unless otherwise noted.
 #
-class role_base (
-  $repos_array_debian          = undef,
-  $owner_email                 = undef,
-  $packages_attribute_defaults = undef,
-  $packages_hash_debian        = undef,
-  $packages_hash_redhat        = undef,
-  $users_attribute_defaults    = undef,
-  $users_hash                  = undef,
-  ) {
+class role_base {
   
-  class { 'base':
-    repos_array_debian          => $repos_array_debian,
-    owner_email                 => $owner_email,
-    packages_attribute_defaults => $packages_attribute_defaults,
-    packages_hash_debian        => $packages_hash_debian,
-    packages_hash_redhat        => $packages_hash_redhat,
-    users_attribute_defaults    => $users_attribute_defaults,
-    users_hash                  => $users_hash,
-  }
+  include base
 
 }
