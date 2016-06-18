ca725ddc8f9647b94fac80c5c92e3d11c309c1cc
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-06-18 19:42:46 +0200

    Update client-openvpn.pp

--- manifests/client-openvpn.pp
+++ manifests/client-openvpn.pp
@@ -1,9 +1,5 @@
 # == Class: role_base::client-openvpn
 #
-class role_base::client-openvpn (
-  $openvpn_client_hash,
-) {
-
-  create_resources('openvpn_client::client', $openvpn_client_hash)
-
+class role_base::client-openvpn {
+  class { 'role_openvpn': }
 }
