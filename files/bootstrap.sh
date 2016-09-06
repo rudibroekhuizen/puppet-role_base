ebc834c34726b45274cd259a9d40ab01bda07e48
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-09-06 11:12:45 +0200

    Support for Puppet 4

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,25 +1,131 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# This script installs puppet 3.x or 4.x on ubuntu and centos, installs bundler, ruby and the puppet module skeleton
+#
+# Usage:
+# Ubuntu / Debian: wget https://raw.githubusercontent.com/pgomersbach/puppet-module-skeleton/master/install.sh; bash install.sh
+#
+# Red Hat / CentOS: curl https://raw.githubusercontent.com/pgomersbach/puppet-module-skeleton/master/install.sh -o bootstrap.sh; bash install.sh
+# Options: add 3 as parameter to install 4.x release
+# set -e
+# default major version, comment to install puppet 3.x
+PUPPETMAJORVERSION=4
 
-# Update the system
-#sudo apt-get update -y
-#sudo apt-get upgrade -y
+### Code start ###
+function provision_ubuntu {
+    # get release info
+    if [ -f /etc/lsb-release ]; then
+      . /etc/lsb-release
+    else
+      DISTRIB_CODENAME=$(lsb_release -c -s)
+    fi
+    if [ $PUPPETMAJOR -eq 4 ]; then
+      REPO_DEB_URL="http://apt.puppetlabs.com/puppetlabs-release-pc1-${DISTRIB_CODENAME}.deb"
+      AGENTNAME="puppet-agent"
+    else
+      REPO_DEB_URL="http://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb"
+      AGENTNAME="puppet"
+    fi
 
-# Install Puppet and Git on osfamily debian
-if [ -f /usr/bin/dpkg ];then
-  wget http://apt.puppetlabs.com/puppetlabs-release-stable.deb
-  dpkg -i puppetlabs-release-stable.deb
-  apt-get --yes --quiet update
-  apt-get --yes -o Dpkg::Options::="--force-confold" --quiet install git puppet-common ruby1.9.1 libaugeas-ruby
+    # Update the system
+    sudo apt-get update -y
+    
+    # Set locale
+    sudo locale-gen nl_NL.UTF-8
+    
+    # Configure repos
+    echo "Configuring PuppetLabs repo..."
+    repo_deb_path=$(mktemp)
+    wget --output-document="${repo_deb_path}" "${REPO_DEB_URL}" 2>/dev/null
+    sudo dpkg -i "${repo_deb_path}" >/dev/null
+    echo "Install ruby ppa"
+    sudo apt-get install -y software-properties-common >/dev/null
+    sudo apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null
+    sudo apt-get update >/dev/null
+    
+    # Install Puppet
+    echo "Installing Puppet..."
+    sudo apt-get -y install git $AGENTNAME >/dev/null
+    echo "Puppet installed!"
+    echo "Install development packages"
+    sudo apt-get -y install ruby2.2 ruby2.2-dev bundler libxslt-dev libxml2-dev zlib1g-dev >/dev/null
+    return 0
+}
+
+function provision_rhel() {
+    # Get release info
+    grep -i "7" /etc/redhat-release
+    if [ $? -eq 0 ]; then
+      RHMAJOR=7
+    fi
+    grep -i "6" /etc/redhat-release
+    if [ $? -eq 0 ]; then
+      RHMAJOR=6
+    fi
+    if [ $PUPPETMAJOR -eq 4 ]; then
+      REPO_RPM_URL="http://yum.puppetlabs.com/puppetlabs-release-pc1-el-${RHMAJOR}.noarch.rpm"
+      AGENTNAME="puppet-agent"
+    else
+      REPO_RPM_URL="http://yum.puppetlabs.com/puppetlabs-release-el-${RHMAJOR}.noarch.rpm"
+      AGENTNAME="puppet"
+    fi
+    sudo yum install -y wget git > /dev/null
+
+    # Configure repos
+    echo "Configuring PuppetLabs repo..."
+    repo_rpm_path=$(mktemp)
+    wget --output-document="${repo_rpm_path}" "${REPO_RPM_URL}" 2>/dev/null
+    sudo rpm -i "${repo_rpm_path}" >/dev/null
+
+    # Install puppet
+    echo "Installing Puppet..."
+    sudo yum install -y $AGENTNAME >/dev/null
+    echo "Puppet installed!"
+    echo "Install development packages"
+    sudo yum install -y ruby2.2 ruby2.2-dev bundler libxslt-dev libxml2-dev zlib1g-dev >/dev/null
+    return 0
+}
+
+if [ "$#" -gt 0 ]; then
+   if [ "$1" = 3 ]; then
+     PUPPETMAJOR=3
+   else
+     PUPPETMAJOR=4
+  fi
+else
+  PUPPETMAJOR=$PUPPETMAJORVERSION
+fi
+echo $PUPPETMAJOR
+
+grep -i "ubuntu" /etc/issue
+if [ $? -eq 0 ]; then
+    provision_ubuntu
+fi
+grep -i "Debian" /etc/issue
+if [ $? -eq 0 ]; then
+    provision_ubuntu
 fi
 
-# Install Puppet and Git on osfamily redhat
-if [ -f /bin/rpm ];then
-  rpm -Uhv --force http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
-  curl -O http://apt.sw.be/redhat/el6/en/x86_64/extras/RPMS/git-1.7.10-1.el6.rfx.x86_64.rpm
-  curl -O http://apt.sw.be/redhat/el6/en/x86_64/extras/RPMS/perl-Git-1.7.10-1.el6.rfx.x86_64.rpm
-  yum -y install perl-DBI rsync
-  rpm -i --force git-1.7.10-1.el6.rfx.x86_64.rpm perl-Git-1.7.10-1.el6.rfx.x86_64.rpm
-  yum -y install puppet
+if [ -f /etc/redhat-release ]; then
+  grep -i "Red Hat" /etc/redhat-release
+  if [ $? -eq 0 ]; then
+      provision_rhel
+  fi
+  grep -i "CentOS" /etc/redhat-release
+  if [ $? -eq 0 ]; then
+      provision_rhel
+  fi
+fi
+
+if [ $PUPPETMAJOR -eq 4 ]; then
+    # make symlinks
+    echo "Set symlinks"
+    FILES="/opt/puppetlabs/bin/*"
+    for f in $FILES
+    do
+      filename="${f##*/}"
+      sudo ln -f -s "$f" "/usr/local/bin/${filename}"
+    done
 fi
 
 # Install r10k
