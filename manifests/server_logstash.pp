0bf6d73375889870edf517e22d59940152cd105b
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-10-02 11:12:20 +0200

    Update server_logstash.pp

--- manifests/server_logstash.pp
+++ manifests/server_logstash.pp
@@ -1,6 +1,8 @@
 # == Class: role_base::server_logstash
 #
 class role_base::server_logstash {
-  class { 'base': } ->
-  class { 'role_logstash': }
+  include base
+  class { 'role_logstash': 
+    require base
+  }
 }
