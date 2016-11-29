3445778515b3bce9ea1fde125fb00d178ecfef6c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-11-29 23:13:37 +0100

    Update server_drupal.pp

--- manifests/server_drupal.pp
+++ manifests/server_drupal.pp
@@ -6,8 +6,6 @@ class role_base::server_drupal {
   class { 'role_mysql': }
   class { 'role_php': }
 
-  include composer
-
   drush::drush { 'drush8':
     version   => '8',
     link_name => '/usr/local/bin/drush8',
