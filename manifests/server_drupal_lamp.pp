b6bb02c826b4ff574c8a4e353315b093577515d6
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-12-08 15:39:00 +0100

    Update server_drupal_lamp.pp

--- manifests/server_drupal_lamp.pp
+++ manifests/server_drupal_lamp.pp
@@ -6,11 +6,19 @@ class role_base::server_drupal_lamp {
     require => Stage["main"],
   }
 
-  class { 'role_apache': } ->
-  class { 'role_mysql': } ->
-  class { 'role_php': } ->
+  class { 'role_apache': }
+  class { 'role_mysql': }
+  class { 'role_php': }
   class { 'drush': 
     stage => post
+  }
+  
+  package { 'libapache2-mod-php5':
+     ensure => 'purged',
+  }->
+
+  package { 'libapache2-mod-php7.0':
+    ensure => 'installed',
   }
 
 }
