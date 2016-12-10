82209584546082f1876e8e86216c6b314594e7ed
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-12-10 11:35:34 +0100

    Update server_drupal_lamp.pp

--- manifests/server_drupal_lamp.pp
+++ manifests/server_drupal_lamp.pp
@@ -2,15 +2,24 @@
 #
 class role_base::server_drupal_lamp {
 
+  stage { 'pre':
+    before => Stage["main"],
+  }
+
   stage { 'post':
     require => Stage["main"],
   }
 
-  class { 'role_php': } ->
-  class { 'role_mysql': } ->
+  # Pre
+  class { 'role_php':
+    stage => pre
+  }
+  
+  # Main
+  class { 'role_mysql': }
   class { 'role_apache': }
   
-  
+  # Post
   class { 'drush': 
     stage => post
   }
