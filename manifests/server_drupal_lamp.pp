a3c9e8c35c9041d19953f030c58f3ac022c6ec36
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-12-10 21:08:58 +0100

    Update server_drupal_lamp.pp

--- manifests/server_drupal_lamp.pp
+++ manifests/server_drupal_lamp.pp
@@ -14,7 +14,13 @@ class role_base::server_drupal_lamp {
   class { 'role_mysql': }
   class { 'role_apache': }
   
-  # Post
+  # Apache php 7 modules after php is installed
+  class {'::apache::mod::php':
+    php_version => '7.0',
+    stage       => post
+  }
+  
+  # Install Drush
   class { 'drush': 
     stage => post
   }
