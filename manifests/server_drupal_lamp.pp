93765dbb3ac8102096b67e9ecd2408aaac05ad58
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-12-10 12:56:22 +0100

    Update server_drupal_lamp.pp

--- manifests/server_drupal_lamp.pp
+++ manifests/server_drupal_lamp.pp
@@ -12,11 +12,7 @@ class role_base::server_drupal_lamp {
 
   class { 'role_php': }
   class { 'role_mysql': }
-  
-  # Require php::repo needed for Apache PhP extenstions (libapache2-mod-php7.0, php7.0-mysql, etc)
-  class { 'role_apache': 
-    require => Class['::php::repo']
-  }
+  class { 'role_apache': }
   
   # Post
   class { 'drush': 
