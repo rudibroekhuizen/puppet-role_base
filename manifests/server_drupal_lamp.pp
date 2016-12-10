70ce03474c962164abede427749ea8c633fd1a73
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-12-10 11:12:25 +0100

    Update server_drupal_lamp.pp

--- manifests/server_drupal_lamp.pp
+++ manifests/server_drupal_lamp.pp
@@ -9,12 +9,9 @@ class role_base::server_drupal_lamp {
   class { 'role_apache': }
   class { 'role_mysql': }
   class { 'role_php': }
+  
   class { 'drush': 
     stage => post
   }
   
-  #package { 'libapache2-mod-php7.0':
-  #  ensure => 'installed',
-  #}
-
 }
