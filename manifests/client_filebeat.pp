dad84836b2cea244cf7357f2fcb9568e2b1217a1
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-01-10 14:24:50 +0100

    Update client_filebeat.pp

--- manifests/client_filebeat.pp
+++ manifests/client_filebeat.pp
@@ -1,5 +1,16 @@
 # == Class: role_base::client_filebeat
 #
 class role_base::client_filebeat {
-  class { 'filebeat': }
+
+  #class { 'filebeat': }
+
+  apt::source { 'beats':
+    location => 'https://artifacts.elastic.co/packages/5.x/apt',
+    release  => 'stable',
+    repos    => 'main',
+    key      => {
+      id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
+      source => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+    }
+  }
 }
