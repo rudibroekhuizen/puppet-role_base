9ac9918372fb391384eceee8d360777bf039d9af
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-03-31 10:18:54 +0200

    Create server_waarneming_web.pp

---
+++ manifests/server_waarneming_web.pp
@@ -0,0 +1,10 @@
+# == Class: role_base::server_waarneming_web
+#
+class role_base::server_waarneming_web {
+  class { 'role_waarneming::common': }
+  class { 'role_waarneming::conf': }
+  class { 'role_waarneming::django_app': }
+  class { 'role_waarneming::mail': }
+  class { 'role_waarneming::pgbouncer': }
+  class { 'role_waarneming::php_app': }
+}
