9582dc0115adeff04b43a2689c86cfc5cfcdcca3
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-04-07 10:06:28 +0200

    Update server_waarneming_db.pp

--- manifests/server_waarneming_db.pp
+++ manifests/server_waarneming_db.pp
@@ -1,7 +1,11 @@
 # == Class: role_base::server_waarneming_db
 #
 class role_base::server_waarneming_db {
+  
   class { 'role_waarneming::common': }
   class { 'role_waarneming::conf': }
   class { 'role_waarneming::db': }
+  
+  include filebeat
+  
 }
