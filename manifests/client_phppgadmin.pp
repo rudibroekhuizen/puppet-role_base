3d0629b1db11d0fe189af93c70585348a7051e74
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-04-11 16:04:51 +0200

    Create client_phppgadmin.pp

---
+++ manifests/client_phppgadmin.pp
@@ -0,0 +1,23 @@
+# == Class role_base::client_phppgadmin
+#
+class role_base::client_phppgadmin (
+  $path     = '/srv/phppgadmin',
+  $user     = 'www-data',
+  $servers  = [],
+  $revision = 'origin/REL_5-1'
+) {
+
+  file { $path:
+    ensure         => directory,
+    owner          => $user,
+  } ->
+  
+  vcsrepo { $path:
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/phppgadmin/phppgadmin.git',
+    user     => $user,
+    revision => $revision,
+  }
+  
+}
