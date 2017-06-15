b2ba6baa83d14ffe6dc5af961732e9782beb141c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-15 16:50:23 +0200

    Create server_elk.pp

--- manifests/server_elk.pp
+++ manifests/server_elk.pp
@@ -1,6 +1,19 @@
 # == Class: role_base::server_elk
 #
 class role_base::server_elk {
+
+  concat::fragment{ 'common':
+    target => /etc/puppetlabs/puppet/Puppetfile,
+    source => 'puppet:///modules/role_base/files/Puppetfile',
+    order  => '01'
+  }
+
+  concat::fragment{ 'specific':
+    target => /etc/puppetlabs/puppet/Puppetfile,
+    source => 'puppet:///modules/server_elk/files/Puppetfile',
+    order  => '02'
+  }
+  
   class { 'role_elasticsearch': }
   class { 'role_logstash': }
   class { 'kibana5': }
