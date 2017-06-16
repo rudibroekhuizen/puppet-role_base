7c4aa5f7fe47eb07fcb0d92e659b643d195a9f0c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-16 16:36:21 +0200

    Update server_elk.pp

--- manifests/server_elk.pp
+++ manifests/server_elk.pp
@@ -2,16 +2,11 @@
 #
 class role_base::server_elk {
 
-  concat::fragment{ 'common':
-    target => /etc/puppetlabs/puppet/Puppetfile,
-    source => 'puppet:///modules/role_base/files/Puppetfile',
-    order  => '01'
-  }
-
-  concat::fragment{ 'specific':
-    target => /etc/puppetlabs/puppet/Puppetfile,
-    source => 'puppet:///modules/server_elk/files/Puppetfile',
-    order  => '02'
+  $vcsrepo_hash.each |$title, $vcsrepo| {
+    vcsrepo { $title:
+      provider => git,
+      source   => $vcsrepo["source"],
+    }
   }
   
   class { 'role_elasticsearch': }
