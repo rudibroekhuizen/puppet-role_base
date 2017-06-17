67d270f0a99af5c1e17f60fbc532c5b4f85520c2
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-17 11:23:54 +0200

    Update server_elk.pp

--- manifests/server_elk.pp
+++ manifests/server_elk.pp
@@ -2,18 +2,11 @@
 #
 class role_base::server_elk {
 
-  # Override default modules loaded by Puppetfile
-  $vcsrepo_hash.each |$title, $vcsrepo| {
-    vcsrepo { "/etc/puppetlabs/code/modules/${title}":
-      provider => git,
-      source   => $vcsrepo["source"],
-    }
-  }
-  
-  class { 'role_elasticsearch': }
+
+  class { 'misc::vcsrepo': }
+  #class { 'role_elasticsearch': }
   class { 'role_logstash': }
   class { 'kibana5': }
   
   #class { 'role_rsyslog': }
 }
-
