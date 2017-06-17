119df8896284f3de71194ecd349ca461749314ab
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-17 11:53:27 +0200

    Update server_elk.pp

--- manifests/server_elk.pp
+++ manifests/server_elk.pp
@@ -2,11 +2,17 @@
 #
 class role_base::server_elk {
 
+  stage { 'pre':
+    before => Stage["main"],
+  }
 
-  class { 'misc::vcsrepo': }
+
+  class { 'misc::vcsrepo': 
+    stage => pre
+  }
   #class { 'role_elasticsearch': }
   class { 'role_logstash': }
-  class { 'kibana5': }
+  #class { 'kibana5': }
   
   #class { 'role_rsyslog': }
 }
