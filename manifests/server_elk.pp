c99e8749432c0766796bc7a7b0654cc8ec274dda
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-18 19:07:27 +0200

    Update server_elk.pp

--- manifests/server_elk.pp
+++ manifests/server_elk.pp
@@ -2,17 +2,17 @@
 #
 class role_base::server_elk {
 
-  stage { 'pre':
-    before => Stage["main"],
-  }
+  #stage { 'pre':
+  #  before => Stage["main"],
+  #}
 
   # Download Puppet modules from Github
-  class { 'misc::vcsrepo': 
-    stage => pre
-  }
+  #class { 'misc::vcsrepo': 
+  #  stage => pre
+  #}
 
-  #class { 'role_elasticsearch': }
-  class { 'role_logstash': }
+  class { 'role_elasticsearch': }
+  #class { 'role_logstash': }
   #class { 'kibana5': }
   
   #class { 'role_rsyslog': }
