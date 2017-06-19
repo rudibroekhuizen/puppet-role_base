a07f6a9cbb6138355ae1b0f19bfb7ae822a2e09f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-19 09:37:15 +0200

    Update bootstrap.sh

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -133,4 +133,6 @@ fi
 puppet apply -e 'include base'
 
 # Apply manifest as noted in data_source yaml file, hiera_include includes classes from every level of the hiera hierarchy
-puppet apply -e 'hiera_include('classes')'
+if [ -n "$1" ];then
+  puppet apply -e 'hiera_include('classes')'
+fi
