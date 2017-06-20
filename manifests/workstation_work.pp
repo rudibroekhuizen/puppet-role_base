b00ef37369249d99d5e6bb0ab830da615afa96ea
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-20 10:35:52 +0200

    Update workstation_work.pp

--- manifests/workstation_work.pp
+++ manifests/workstation_work.pp
@@ -1,6 +1,9 @@
 # == Class: role_base::workstation_work
 #
 class role_base::workstation_work {
+  class { 'docker':
+    version => 'latest',
+  }
   #class { 'sqlplus': }
   #class { 'role_burp': }
   #class { 'role_tftp': }
