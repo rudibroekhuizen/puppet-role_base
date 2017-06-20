1cb1ebfd2ee098474cd09bd92ac9d77d8467bd0f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-06-20 09:46:39 +0200

    Update workstation_work.pp

--- manifests/workstation_work.pp
+++ manifests/workstation_work.pp
@@ -1,6 +1,14 @@
 # == Class: role_base::workstation_work
 #
 class role_base::workstation_work {
+
+# Puppet modules needed
+base::vcsrepo_hash:
+  'elasticsearch':
+    source: 'https://github.com/elastic/puppet-elasticsearch'
+  'role_elasticsearch':
+    source: 'https://github.com/rudibroekhuizen/puppet-role_elasticsearch'
+
   #class { 'sqlplus': }
   #class { 'role_burp': }
   #class { 'role_tftp': }
