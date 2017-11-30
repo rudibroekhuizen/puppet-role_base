6572b6d8254b8854a425d8945111d3f343a9b23b
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-11-30 13:44:14 +0100

    Create server_waarneming_db_slave.pp

---
+++ manifests/server_waarneming_db_slave.pp
@@ -0,0 +1,9 @@
+# == Class: role_base::server_waarneming_db
+#
+class role_base::server_waarneming_db {
+  
+  class { 'role_waarneming::common': }
+  class { 'role_waarneming::conf': }
+  class { 'role_waarneming::db_slave': }
+  
+}
