a560df1f7ed01797b979dcfeb6993b623744cea5
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2019-05-18 21:26:48 +0200

    Update server_postgresql.pp

--- manifests/server_postgresql.pp
+++ manifests/server_postgresql.pp
@@ -3,6 +3,17 @@
 class role_base::server_postgresql {
 
   class { 'postgresql::server': }
-  class { 'postgresql::globals': }
+
+  $config_entry = hiera_hash('postgresql::server::config_entry', {})
+  create_resources(postgresql::server::config_entry, $config_entry)
+
+  $pg_hba_rule = hiera_hash('postgresql::server::pg_hba_rule', {})
+  create_resources(postgresql::server::pg_hba_rule, $pg_hba_rule)
+
+  $role = hiera_hash('postgresql::server::role', {})
+  create_resources(postgresql::server::role, $role)
+
+  $db = hiera_hash('postgresql::server::db', {})
+  create_resources(postgresql::server::db, $db)
  
 }
